a4e0005dea6bb8b4aa1e48f288e6f7d009c40c85
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-11-23 17:41:15 +0300

    Practice

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return(
+    <div>
+      <MesaVerde />
+      <h2>Trees from Rocky Mountain: {trees}</h2>
+    </div>
+  )
 }
+
+export default ColoradoStateParks
